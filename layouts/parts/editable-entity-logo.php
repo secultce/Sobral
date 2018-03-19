14d35e3b42f8019b3128cc73c3c2e4a7bcc33976
Nalomy de Souza A. Oliveira <nalomy.souza@secult.ce.gov.br>
2018-03-19 19:39:13 -0300

    Tema Prefeitura de Sobral

--- layouts/parts/editable-entity-logo.php
+++ layouts/parts/editable-entity-logo.php
@@ -1,1 +1,4 @@
-<h1 id="small-brand"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-sobral-cultura.png'); ?>" /></a></h1>
+<h1 id="small-brand">
+	<a href="<?php echo $app->getBaseUrl() ?>"><img
+		src="<?php $this->asset('img/logo-sobral-cultura.png'); ?>" /></a>
+</h1>
--- layouts/parts/header-logo.php
+++ layouts/parts/header-logo.php
@@ -1,1 +1,4 @@
-<h1 id="brand-logo"><a href="<?php echo $app->getBaseUrl() ?>"><img src="<?php $this->asset('img/logo-sobral-cultura.png'); ?>" /></a></h1>
+<h1 id="brand-logo">
+	<a href="<?php echo $app->getBaseUrl() ?>"><img
+		src="<?php $this->asset('img/logo-sobral-cultura.png'); ?>" /></a>
+</h1>
